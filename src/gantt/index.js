b3ceaaef8f73557aea150f656e7d93012741ce58
nawin <kim.nawin@gmail.com>
2021-02-13 16:13:49 +0100

    show month separator according to number of days in each month

--- src/gantt/index.js
+++ src/gantt/index.js
@@ -1,79 +1,83 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import GanttJS from "frappe-gantt";
-import GanttJS from "./gantt";
-import { bind, clear } from "size-sensor";
+import GanttJS from './gantt';
+import { bind, clear } from 'size-sensor';
 // import { noop } from './helper';
 
 const noop = () => {};
 
 export default class ReactGantt extends Component {
-  ganttRef = undefined;
+    ganttRef = undefined;
 
-  componentDidMount() {
-    this.renderFrappeGanttDOM();
-  }
-
-  // redraw the gantt when update. now change the viewMode
-  componentDidUpdate(prevProps, prevState) {
-    if (this.ganttInst) {
-      this.ganttInst.refresh(this.props.tasks);
-      // 不相等才刷新
-      if (this.props.viewMode !== prevProps.viewMode) {
-        this.ganttInst.change_view_mode(this.props.viewMode);
-      }
+    componentDidMount() {
+        this.renderFrappeGanttDOM();
     }
-  }
 
-  componentWillUnmount() {
-    clear(this.ganttRef);
-  }
+    // redraw the gantt when update. now change the viewMode
+    componentDidUpdate(prevProps, prevState) {
+        if (this.ganttInst) {
+            this.ganttInst.refresh(this.props.tasks);
+            // 不相等才刷新
+            if (this.props.viewMode !== prevProps.viewMode) {
+                this.ganttInst.change_view_mode(this.props.viewMode);
+            }
+        }
+    }
 
-  /**
-   * render the gantt chart
-   * @returns {GanttJS}
-   */
-  renderFrappeGanttDOM() {
-    // init the Gantt
-    // if exist, return
-    if (this.ganttInst) return this.ganttInst;
+    componentWillUnmount() {
+        clear(this.ganttRef);
+    }
 
-    const {
-      onClick,
-      onDateChange,
-      onProgressChange,
-      onViewChange,
-      customPopupHtml,
-      tasks,
-      viewMode,
-    } = this.props;
+    /**
+     * render the gantt chart
+     * @returns {GanttJS}
+     */
+    renderFrappeGanttDOM() {
+        // init the Gantt
+        // if exist, return
+        if (this.ganttInst) return this.ganttInst;
 
-    // when resize
-    bind(this.ganttRef, () => {
-      if (this.ganttInst) {
-        this.ganttInst.refresh(this.props.tasks);
-      }
-    });
+        const {
+            onClick,
+            onDateChange,
+            onProgressChange,
+            onViewChange,
+            customPopupHtml,
+            tasks,
+            viewMode,
+        } = this.props;
 
-    // new instance
-    this.ganttInst = new GanttJS(this.ganttRef, tasks, {
-      on_click: onClick || noop,
-      on_date_change: onDateChange || noop,
-      on_progress_change: onProgressChange || noop,
-      on_view_change: onViewChange || noop,
-      custom_popup_html: customPopupHtml || null,
-    });
-    // change view mode
-    this.ganttInst.change_view_mode(viewMode);
-    return this.ganttInst;
-  }
+        // when resize
+        bind(this.ganttRef, () => {
+            if (this.ganttInst) {
+                this.ganttInst.refresh(this.props.tasks);
+            }
+        });
 
-  render() {
-    return (
-      <svg
-        ref={(node) => {
-          this.ganttRef = node;
-        }}
-      />
-    );
-  }
+        // new instance
+        this.ganttInst = new GanttJS(this.ganttRef, tasks, {
+            bar_height: 40,
+            bar_corner_radius: 3,
+            arrow_curve: 10,
+            // step: 1,
+            on_click: onClick || noop,
+            on_date_change: onDateChange || noop,
+            on_progress_change: onProgressChange || noop,
+            on_view_change: onViewChange || noop,
+            custom_popup_html: customPopupHtml || null,
+        });
+        // change view mode
+        this.ganttInst.change_view_mode(viewMode);
+        return this.ganttInst;
+    }
+
+    render() {
+        return (
+            <svg
+                ref={(node) => {
+                    this.ganttRef = node;
+                }}
+            />
+        );
+    }
 }
